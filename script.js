b270113195b320a5362618817a8afd7472110a84
mehdjdjsjaks <m7088696@gmail.com>
2025-04-12 23:23:30 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,47 @@
-// کانفیگ تو
+// Import Firebase modules
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
+
+// Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyAUFJeLyvFxW2iQ143snxn0d_9-36zzb1Q",
   authDomain: "axcel-20956.firebaseapp.com",
   projectId: "axcel-20956",
-  storageBucket: "axcel-20956.firebasestorage.app",
+  storageBucket: "axcel-20956.appspot.com",
   messagingSenderId: "115833840953",
-  appId: "1:115833840953:web:03194ff9d298b5d2bf74e8",
-  measurementId: "G-2NSQVB63JG"
+  appId: "1:115833840953:web:03194ff9d298b5d2bf74e8"
 };
 
-// اتصال به Firebase
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
+// Init Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-function signUp() {
+// Sign up
+window.signUp = function () {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
-  auth.createUserWithEmailAndPassword(email, password)
-    .then(userCredential => {
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(() => {
       alert("ثبت‌نام موفق بود!");
+      window.location.href = "chat.html";
     })
-    .catch(error => {
-      alert("خطا: " + error.message);
+    .catch((error) => {
+      alert(error.message);
     });
-}
+};
 
-function login() {
+// Sign in
+window.signIn = function () {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
-  auth.signInWithEmailAndPassword(email, password)
-    .then(userCredential => {
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
       alert("ورود موفق بود!");
+      window.location.href = "chat.html";
     })
-    .catch(error => {
-      alert("خطا: " + error.message);
+    .catch((error) => {
+      alert(error.message);
     });
-}
+};
